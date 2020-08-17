b1407c4d2f48881104931334254bf807c3e08bcb
Paul Freeman <paul.freeman.cs@gmail.com>
2020-08-17 14:28:44 +1200

    output id

--- cmd/autonat/main.go
+++ cmd/autonat/main.go
@@ -15,15 +15,17 @@ const port = 33477
 
 func main() {
 	ctx := context.Background()
-	if _, err := libp2p.New(ctx,
+	h, err := libp2p.New(ctx,
 		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%v", port)),
 		libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) { return dht.New(ctx, h) }),
 		libp2p.EnableNATService(),
 		libp2p.EnableRelay(relay.OptHop),
 		libp2p.EnableAutoRelay(),
-	); err != nil {
+	)
+	if err != nil {
 		panic(err)
 	}
 	fmt.Println("I am an autonat service and I am ready to go!")
+	fmt.Printf("My peer ID is %v", h.ID())
 	select {}
 }
