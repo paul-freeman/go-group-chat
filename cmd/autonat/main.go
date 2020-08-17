a427fb0d6bd3d42567251186382e05aad34a6a09
Paul Freeman <paul.freeman.cs@gmail.com>
2020-08-17 12:54:25 +1200

    add public IP

--- cmd/autonat/main.go
+++ cmd/autonat/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"flag"
+	"strings"
 
 	"github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p"
@@ -9,6 +11,7 @@ import (
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/routing"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/multiformats/go-multiaddr"
 )
 
 var logger = log.Logger("autonat")
@@ -18,6 +21,13 @@ func main() {
 	_ = log.SetLogLevel("addrutil", "info")
 	_ = log.SetLogLevel("basichost", "info")
 
+	// check command line arguments
+	a := flag.String("public-ip", "", "the public `IP` address of this autonat peer")
+	flag.Parse()
+	if a == nil || *a == "" {
+		logger.Fatal("could not read autonat ip address")
+	}
+
 	ctx := context.Background()
 
 	h, err := libp2p.New(ctx,
@@ -25,15 +35,30 @@ func main() {
 		libp2p.EnableNATService(),
 		libp2p.EnableRelay(relay.OptHop),
 		libp2p.EnableAutoRelay(),
+		libp2p.AddrsFactory(func(addrs []multiaddr.Multiaddr) []multiaddr.Multiaddr {
+			for _, addr := range addrs {
+				if strings.Contains(addr.String(), "127.0.0.1") {
+					p := strings.Replace(addr.String(), "127.0.0.1", *a, 1)
+					ma, err := multiaddr.NewMultiaddr(p)
+					if err != nil {
+						continue
+					}
+					return []multiaddr.Multiaddr{ma}
+				}
+			}
+			logger.Fatal("could not find any multiaddrs")
+			return []multiaddr.Multiaddr{}
+		}),
 	)
 	if err != nil {
 		logger.Fatalf("could not start libp2p host: %v", err)
 	}
 
 	logger.Infof("I am a service peer!")
-	logger.Infof("Connect to me at:")
-	for i, a := range h.Addrs() {
-		logger.Infof("%d\t%v/p2p/%v", i+1, a, h.ID())
+	for _, addr := range h.Addrs() {
+		if strings.Contains(addr.String(), *a) {
+			logger.Infof("Connect to me at: %v", addr)
+		}
 	}
 	select {}
 }
