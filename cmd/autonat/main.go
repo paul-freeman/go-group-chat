faaf2c797b1cc67ec9fc6eb971444fefe24af377
Paul Freeman <paul.freeman.cs@gmail.com>
2020-08-17 14:26:02 +1200

    refactoring

--- cmd/autonat/main.go
+++ cmd/autonat/main.go
@@ -2,63 +2,28 @@ package main
 
 import (
 	"context"
-	"flag"
-	"strings"
+	"fmt"
 
-	"github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p"
 	relay "github.com/libp2p/go-libp2p-circuit"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/routing"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	"github.com/multiformats/go-multiaddr"
 )
 
-var logger = log.Logger("autonat")
+const port = 33477
 
 func main() {
-	log.SetAllLoggers(log.LevelDebug)
-	_ = log.SetLogLevel("addrutil", "info")
-	_ = log.SetLogLevel("basichost", "info")
-
-	// check command line arguments
-	a := flag.String("public-ip", "", "the public `IP` address of this autonat peer")
-	flag.Parse()
-	if a == nil || *a == "" {
-		logger.Fatal("could not read autonat ip address")
-	}
-
 	ctx := context.Background()
-
-	h, err := libp2p.New(ctx,
+	if _, err := libp2p.New(ctx,
+		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%v", port)),
 		libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) { return dht.New(ctx, h) }),
 		libp2p.EnableNATService(),
 		libp2p.EnableRelay(relay.OptHop),
 		libp2p.EnableAutoRelay(),
-		libp2p.AddrsFactory(func(addrs []multiaddr.Multiaddr) []multiaddr.Multiaddr {
-			for _, addr := range addrs {
-				if strings.Contains(addr.String(), "127.0.0.1") {
-					p := strings.Replace(addr.String(), "127.0.0.1", *a, 1)
-					ma, err := multiaddr.NewMultiaddr(p)
-					if err != nil {
-						continue
-					}
-					return []multiaddr.Multiaddr{ma}
-				}
-			}
-			logger.Fatal("could not find any multiaddrs")
-			return []multiaddr.Multiaddr{}
-		}),
-	)
-	if err != nil {
-		logger.Fatalf("could not start libp2p host: %v", err)
+	); err != nil {
+		panic(err)
 	}
-
-	logger.Infof("I am a service peer!")
-	for _, addr := range h.Addrs() {
-		if strings.Contains(addr.String(), *a) {
-			logger.Infof("Connect to me at: %v/p2p/%v", addr, h.ID())
-		}
-	}
+	fmt.Println("I am an autonat service and I am ready to go!")
 	select {}
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"strings"
 	"time"
 
 	"github.com/ipfs/go-log/v2"
@@ -12,7 +13,7 @@ import (
 	"github.com/libp2p/go-libp2p-core/routing"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/config"
-	ma "github.com/multiformats/go-multiaddr"
+	"github.com/multiformats/go-multiaddr"
 )
 
 var logger = log.Logger("group-chat")
@@ -23,18 +24,24 @@ func main() {
 	_ = log.SetLogLevel("basichost", "info")
 
 	// check command line arguments
-	a := flag.String("autonat", "", "the P2P multiaddr of the autonat peer")
+	a := flag.String("autonat", "", "a comma-separated list of multiaddrs")
 	flag.Parse()
 	if a == nil || *a == "" {
 		logger.Fatal("could not read autonat address")
 	}
-	mAddr, err := ma.NewMultiaddr(*a)
-	if err != nil {
-		logger.Fatalf("could not convert %s to a multiaddr: %v", *a, err)
-	}
-	p2pAddr, err := peer.AddrInfoFromP2pAddr(mAddr)
-	if err != nil {
-		logger.Fatalf("could not convert %s to a P2P address: %v", *a, err)
+	infos := []*peer.AddrInfo{}
+	for _, addr := range strings.Split(*a, ",") {
+		mAddr, err := multiaddr.NewMultiaddr(addr)
+		if err != nil {
+			logger.Errorf("could not convert %s to a multiaddr: %v", *a, err)
+			continue
+		}
+		p2pAddr, err := peer.AddrInfoFromP2pAddr(mAddr)
+		if err != nil {
+			logger.Errorf("could not convert %s to a P2P address: %v", *a, err)
+			continue
+		}
+		infos = append(infos, p2pAddr)
 	}
 
 	// start the application context
@@ -42,17 +49,12 @@ func main() {
 
 	// start libp2p host
 	h, err := libp2p.New(ctx,
-		libp2p.Routing(makeDhtRouting(ctx, *p2pAddr)),
+		libp2p.Routing(makeDhtRouting(ctx, infos)),
 		libp2p.EnableNATService(),
 		libp2p.EnableAutoRelay(),
 	)
 	if err != nil {
 		logger.Fatalf("could not start libp2p host: %v", err)
-	}
-
-	err = h.Connect(ctx, *p2pAddr)
-	if err != nil {
-		panic(err)
 	}
 
 	// monitor our multiaddrs
@@ -67,17 +69,19 @@ func main() {
 	}
 }
 
-func makeDhtRouting(ctx context.Context, bootstrap peer.AddrInfo) config.RoutingC {
+func makeDhtRouting(ctx context.Context, bootstraps []*peer.AddrInfo) config.RoutingC {
 	return func(h host.Host) (routing.PeerRouting, error) {
 		d, err := dht.New(ctx, h)
 		if err != nil {
 			return nil, err
 		}
-		err = h.Connect(ctx, bootstrap)
-		if err != nil {
-			return nil, err
+		for _, bootstrap := range bootstraps {
+			err = h.Connect(ctx, *bootstrap)
+			if err != nil {
+				logger.Errorf("could not connect to bootstrap node: %v", err)
+				continue
+			}
 		}
 		return d, nil
-
 	}
 }
